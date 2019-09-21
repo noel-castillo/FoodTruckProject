93295b300447cea2f20a4a4f9ecfd72140310382
Noel Castillo <noelcastillo@Administrators-MacBook-Pro.local>
2019-09-21 10:24:19 -0600

    Program satisfies requirements.

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -1,6 +1,5 @@
 package com.skilldistillery.foodtrucks;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class FoodTruck {
@@ -13,16 +12,15 @@ public class FoodTruck {
 
 	private double rating;
 
-	public int foodTruckNumber = 0;
+	private int foodTruckNumber = 0;
 
 	private int numberOfFoodTrucks = 5;
 
 	private FoodTruck[] foodTruck = new FoodTruck[numberOfFoodTrucks];
-	
+
 //	C O N S T R U C T O R S
 
 	public FoodTruck() {
-		
 	}
 
 	public FoodTruck(String name, String foodType, double rating) {
@@ -35,19 +33,24 @@ public class FoodTruck {
 //	M E T H O D S
 
 	public FoodTruck getFoodTruckInfo(Scanner keyboard) {
-		System.out.println("Enter food truck data or quit if done entering data.");
+		System.out.println("Enter food truck data or enter quit to proceed:");
 		System.out.print("Food truck name:");
-		String name = keyboard.next();
-		System.out.print("Food type:");
-		String foodType = keyboard.next();
-		System.out.print("Food truck rating:");
-		double rating = keyboard.nextDouble();
-		FoodTruck nextFoodTruck = new FoodTruck(name, foodType, rating);
-		return nextFoodTruck;
+		String name = keyboard.nextLine();
+		if (name.equals("quit")) {
+			return null;
+		} else {
+			System.out.print("Food type:");
+			String foodType = keyboard.nextLine();
+			System.out.print("Food truck rating:");
+			double rating = keyboard.nextDouble();
+			keyboard.nextLine();
+			FoodTruck nextFoodTruck = new FoodTruck(name, foodType, rating);
+			return nextFoodTruck;
+		}
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public void setName(String name) {
@@ -55,7 +58,7 @@ public class FoodTruck {
 	}
 
 	public String getFoodType() {
-		return foodType;
+		return this.foodType;
 	}
 
 	public void setFoodType(String foodType) {
@@ -63,16 +66,22 @@ public class FoodTruck {
 	}
 
 	public double getRating() {
-		return rating;
+		return this.rating;
 	}
 
 	public void setRating(double rating) {
 		this.rating = rating;
 	}
 
-	public void addFoodTruck(FoodTruck nextFoodTruck) {
-		this.foodTruck[foodTruckNumber] = nextFoodTruck;
-		foodTruckNumber++;
+	public boolean addFoodTruck(FoodTruck nextFoodTruck) {
+		boolean output = false;
+		if (nextFoodTruck != null) {
+			this.foodTruck[foodTruckNumber] = nextFoodTruck;
+			foodTruckNumber++;
+		} else {
+			output = true;
+		}
+		return output;
 	}
 
 	public FoodTruck[] getFoodTrucks() {
@@ -82,52 +91,88 @@ public class FoodTruck {
 		}
 		return foodTruckCopy;
 	}
-	
+
 	public void viewAllFoodTrucks() {
 		for (FoodTruck element : getFoodTrucks()) {
 			if (element != null) {
 				element.displayFoodTrucks();
-			}
+			} 
 		}
 	}
-	
+
 	public String getFoodTruckData() {
 		String output = "Food Truck Name: " + name + ", Food Type: " + foodType + ", Rating: " + rating;
 		return output;
 	}
-	
+
 	public void displayFoodTrucks() {
 		String foodTruckData = getFoodTruckData();
 		System.out.println(foodTruckData);
 	}
-	
+
 	public int displayMenu(Scanner keyboard) {
-		System.out.println("1. List all existing food trucks.");
+		System.out.println("1. List all existing food trucks or press enter to quit.");
 		System.out.println("2. See the average rating of food trucks.");
 		System.out.println("3. Display the highest-rated food truck.");
 		System.out.println("4. Quit the program.");
 		int output = keyboard.nextInt();
 		return output;
 	}
-	
+
+	public void calculateAverageRating() {
+		double average = 0.0;
+		int counter = 0;
+		for (FoodTruck element : getFoodTrucks()) {
+			for (int c = 0; c < foodTruckNumber; c++) {
+				if(element != null) {
+				average += element.getRating();
+				counter++;
+				}
+			}
+		}
+		average /= counter;
+		System.out.print("Average Rating: ");
+		System.out.printf("%4.2f%n", average);
+	}
+
+	public void calculateHighestRating() {
+		double highest = 0.0;
+		String bestTruck = "";
+		for (FoodTruck element : getFoodTrucks()) {
+			for (int c = 0; c < foodTruckNumber; c++) {
+				if(element != null) {
+					if(highest < element.getRating()) {
+						highest = element.getRating();
+						bestTruck = element.getName();
+					}
+				}
+			}
+		}
+		System.out.println(bestTruck + " with a rating of " + highest);
+
+	}
+
 	public boolean displayMenuChoice(int menuChoice) {
 		boolean proceed = true;
-		switch(menuChoice) {
+		switch (menuChoice) {
 		case 1:
 			viewAllFoodTrucks();
 			break;
 		case 2:
+			calculateAverageRating();
 			break;
 		case 3:
+			calculateHighestRating();
 			break;
 		case 4:
 			proceed = false;
 			break;
 		default:
+			System.out.println("Not an option.");
 			break;
-		
+
 		}
-		
+
 		return proceed;
 	}
 }
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -7,22 +7,21 @@ public class FoodTruckApp {
 	public static void main(String[] args) {
 
 		Scanner keyboard = new Scanner(System.in);
-		String repeat = "";
 		FoodTruck app = new FoodTruck();
-		boolean proceed = true;
-		
+		boolean proceed = false;
+
 		do {
 			FoodTruck nextFoodTruck = app.getFoodTruckInfo(keyboard);
-			app.addFoodTruck(nextFoodTruck);
-			System.out.println("Enter \"R\" to enter another food truck." + "\nOtherwise press any key to continue");
-			repeat = keyboard.next().toLowerCase();
-		} while(repeat.equals("r"));
-		
+			proceed = app.addFoodTruck(nextFoodTruck);
+		} while (!proceed);
+
 		do {
-		int menuChoice = app.displayMenu(keyboard);
-		proceed = app.displayMenuChoice(menuChoice);
+			int menuChoice = app.displayMenu(keyboard);
+			proceed = app.displayMenuChoice(menuChoice);
 		} while (proceed);
-		
+
+		System.out.println("Goodbye.");
+
 		keyboard.close();
 	}
 
